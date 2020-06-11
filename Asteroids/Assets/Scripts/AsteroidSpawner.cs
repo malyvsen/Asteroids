84ce52f66f63abe72514c655a0d9758816d46a87
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-06-11 17:15:44 +0200

    make every next asteroid faster

--- Asteroids/Assets/Scripts/AsteroidSpawner.cs
+++ Asteroids/Assets/Scripts/AsteroidSpawner.cs
@@ -7,11 +7,14 @@ public class AsteroidSpawner : MonoBehaviour
 {
     public GameObject asteroid = null;
     public float secondsBetweenSpawns = 4f;
-    public float maxSpeed = 4f;
+    public float baseSpeed = 4f;
+    [Tooltip("Each next asteroid is this much faster")]
+    public float speedup = 1f;
 
 
 
     private float nextSpawnTime = 0f;
+    private int numSpawned = 0;
 
     private void Update()
     {
@@ -19,6 +22,7 @@ public class AsteroidSpawner : MonoBehaviour
         {
             Spawn();
             nextSpawnTime = Time.time + secondsBetweenSpawns;
+            numSpawned++;
         }
     }
 
@@ -28,7 +32,8 @@ public class AsteroidSpawner : MonoBehaviour
     {
         var spawned = Instantiate(asteroid);
         var moving = spawned.GetComponent<Moving>();
-        moving.velocity = Random.insideUnitCircle * maxSpeed;
+        var speed = baseSpeed + numSpawned * speedup;
+        moving.velocity = Random.insideUnitCircle.normalized * speed;
         moving.position = Game.player.position + (Vector2)Universe.instance.bounds.extents;
     }
 }
