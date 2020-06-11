9a5a659305d5766c2ba19aab6874d826a14815d7
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-06-11 18:15:15 +0200

    fix sound effect objects not vanishing after their lifetime

---
+++ Asteroids/Assets/Scripts/TemporaryAudio.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+
+public class TemporaryAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void OnEnable()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying) Destroy(gameObject);
+    }
+}
